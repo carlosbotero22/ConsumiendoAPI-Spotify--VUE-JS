7293b3c4db43b57c18b35b2aeac786660b445a00
carlosbotero22 <monoprograma22@gmail.com>
2023-02-09 17:23:00 -0500

    se agrego banner

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,13 @@
 ****** CARLOS BOTERO ********
 *****************************
 <template>
+  <div>
+    <img class="fondo"
+      src="https://img.freepik.com/psd-premium/promocion-pagina-negocios-icono-spotify-render-3d-plantilla-banner-instagram-redes-sociales_407398-178.jpg?w=2000">
+    <h2 class="m-4">Lo más recientes lanzamientos</h2>
+  </div>
   
+
 </template>
 
 <script>
