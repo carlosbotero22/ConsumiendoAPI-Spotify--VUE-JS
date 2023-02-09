6ac6f3f151ab9c7a8ad60692b5c6a5f60b503399
carlosbotero22 <monoprograma22@gmail.com>
2023-02-09 17:50:38 -0500

    mostrando los más recientes releases en spotify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,14 +2,14 @@
 ****** CARLOS BOTERO ********
 *****************************
 <template>
-  <div>
+  <div class="release">
     <img class="fondo"
       src="https://img.freepik.com/psd-premium/promocion-pagina-negocios-icono-spotify-render-3d-plantilla-banner-instagram-redes-sociales_407398-178.jpg?w=2000">
     <h2 class="m-4">Lo más recientes lanzamientos</h2>
-  </div>
-  
-  <div v-for="value in result" :key="value.id">
-     {{ value.name }}
+
+    <li class="info" v-for="item of result" :key="item.id">
+      {{ item.name }}
+    </li>
   </div>
 </template>
 
@@ -17,31 +17,49 @@
 import axios from "axios";
 
 export default {
-  data: () => ({
-    result: []
-  }),
-  created() {
-    const config = {
-      method: 'get',
-      url: 'https://api.spotify.com/v1/browse/new-releases',
-      headers: {
-        'Authorization': 'Bearer BQCBMcrW_IqeYmDiFXloClhnE5hIO7uhuSS4pWsjBIX1Uty9iMpnMC8PHve3L9B4-pu1qq2MwiB40RNAPMQN_xcQvls4BZp7aWcltFaXeDm5HizcpnOy'
-      }
+  data() {
+    return {
+      result: [],
     };
-    axios(config)
-      .then(function (response) {
-        console.log(response)
-        this.result = response.data.albums.items
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+  },
+  methods: {
+    async fetch() {
+      const config = {
+        method: "get",
+        url: "https://api.spotify.com/v1/browse/new-releases",
+        headers: {
+          Authorization:
+            "Bearer BQCBMcrW_IqeYmDiFXloClhnE5hIO7uhuSS4pWsjBIX1Uty9iMpnMC8PHve3L9B4-pu1qq2MwiB40RNAPMQN_xcQvls4BZp7aWcltFaXeDm5HizcpnOy",
+        },
+      };
+      let data = this.result;
+
+      const response = await axios(config)
+        .then(function ({
+          data: {
+            albums: { items },
+          },
+        }) {
+          return items;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      this.result = response;
+    },
+  },
+  mounted() {
+    this.fetch();
   },
 };
 </script>
 
 
 <style>
+h2 {
+  color: #42b983;
+}
+
 h3 {
   margin: 40px 0 0;
 }
@@ -58,6 +76,12 @@ li {
 
 a {
   color: #42b983;
+}
+
+
+
+.release {
+  background-color: black;
 }
 
 .fondo {
